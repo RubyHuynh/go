1e4301ea2554d56866533fd1511e8c21d2e15e8b
Ruby Huynh <hthn.oo@gmail.com>
2021-05-09 14:36:54 +0000

    error + log

--- golang.org/hellomod/hello/hello.go
+++ golang.org/hellomod/hello/hello.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"tuts/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Ruby H")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if (err!= nil) {
+		log.Fatal(err)
+	}
+	message,err = greetings.Hello("Ruby H")
 	fmt.Println(message)
 }
