d1f8ac01ebdd0227b2edb4d22ddc557acf37d84e
Ruby Huynh <hthn.oo@gmail.com>
2021-05-10 16:05:23 +0000

    golang add go test

---
+++ golang.org/hellomod/greetings/greetings_test.go
@@ -0,0 +1,36 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b`+name+`\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys")=%q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
+
+/**
+[root@24e776f271ff greetings]# go test
+PASS
+ok      tuts/greetings  0.002s
+[root@24e776f271ff greetings]# go test -v
+=== RUN   TestHelloName
+--- PASS: TestHelloName (0.00s)
+=== RUN   TestHelloEmpty
+--- PASS: TestHelloEmpty (0.00s)
+PASS
+ok      tuts/greetings  0.002s
+*/
